3c8eb95b8a6a2d79e44f35ca617f349bc62f65f0
MISADDONS <addons2gavilan@gmail.com>
2023-05-24 20:09:23 +0200

    24 mayo 8 h

--- js/app.js
+++ js/app.js
@@ -1,15 +1,16 @@
 const hamburguesa = document.querySelector(".hamburguesa");
 const navegacion = document.querySelector(".navegacion");
 const enlaces = document.querySelectorAll(".navegacion a");
-console.log(enlaces);
+
 
 document.addEventListener('DOMContentLoaded',()=>{
     MostrarMenu();
     cerrarMenu();
-}); /*para que se cargue la pagina aprisa*/
+});
+ /*para que se cargue la pagina aprisa*/
 
 function MostrarMenu(){
     hamburguesa.addEventListener('click',()=>{
-       navegacion.classList.toggle('ocultar');
+        navegacion.classList.toggle('ocultar');
 });
 }
